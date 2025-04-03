5e0af7e5464f38d85f0b2029363b71fdfaae7900
Sarthak Mohapatra <140512881+sarthakm402@users.noreply.github.com>
2025-04-03 17:12:16 +0530

    Update nlp_ncbi_disease.py

--- nlp_ncbi_disease.py
+++ nlp_ncbi_disease.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 import pandas as pd
 from nltk.tokenize import word_tokenize
-import json
+import json 
 import spacy
 from spacy.training.example import Example
 from spacy.util import minibatch
